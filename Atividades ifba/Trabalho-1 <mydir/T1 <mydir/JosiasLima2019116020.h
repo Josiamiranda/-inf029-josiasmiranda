77f4554bf8640f66bf9f24ff462bbba1c1aefcaa
Josiamiranda <80134941+Josiamiranda@users.noreply.github.com>
2021-07-06 16:05:30 -0300

    Add files via upload

---
+++ Atividades ifba/Trabalho-1 <mydir/T1 <mydir/JosiasLima2019116020.h
@@ -0,0 +1,35 @@
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
+
+//  ----- Orientações gerais -----
+//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  Cada aluno deve renomear esse arquivo para Aluno<MATRICULA>.c
+//  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
+
+//  ----- Dados do Aluno -----
+//  Nome:Josias Miranda
+//  email:jmolima2000@gmail.com
+//  Matrícula:2019116020
+//  Semestre:2
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 12/12/2017
+
+// #################################################
+#ifndef TRABALHO1_JosiasLima2019116020_H
+#define TRABALHO1_JosiasLima2019116020_H
+
+int somar(int x, int y); //função utilizada para testes
+int fatorial(int x); //função utilizada para testes
+int q1(char *data);
+int q2(char *datainicial, char *datafinal, int *qtdDias, int *qtdMeses, int *qtdAnos);
+int q3(char *texto, char c, int isCaseSensitive);
+int q4(char *strTexto, char *strBusca, int posicoes[30]);
+int q5(int num);
+int q6(int numerobase, int numerobusca);
+
+#endif  // TRABALHO1_PRIMEIROULTIMONOMEMATRICULA_H
